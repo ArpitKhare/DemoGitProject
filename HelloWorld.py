3178d49ab74e7569c7749a8c0fe62c11250cb6d7
Arpit Khare <akhare@HW11775.local>
2016-06-10 11:14:21 -0700

    modified file-Scenario2

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,3 @@
-name=raw_input("Enter your name: ")
-print "Hello "+str(name)+"!"
+first_name=raw_input("Enter your first name: ")
+last_name=raw_input("Enter your last name: ")
+print "Hello "+str(first_name)+" "+str(last_name)+"!"
